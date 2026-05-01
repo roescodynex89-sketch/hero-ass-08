78108dbe5438c86fc3f39104736a31c2fec06c5f
roescodynex89-sketch <roescody.ne89x@gmail.com>
2026-05-01 19:22:44 +0600

    better-auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -18,7 +18,6 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      redirectURI: "http://localhost:3000/api/auth/callback/google",
     },
   },
 });
